47f730f973b78b7ad2742e4145852f764cfde754
janitha <visnajanitha@gmail.com>
2023-09-02 20:49:58 +0530

    added a new funtion

--- touchapp.js
+++ touchapp.js
@@ -1,1 +1,16 @@
 alert("helloheloo")
+
+function isPrime(n) {
+    if (n <= 1) {
+      return false;
+    }
+  
+    for (var i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) {
+        return false;
+      }
+    }
+  
+    return true;
+  }
+  
